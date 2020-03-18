b8f18715b6ef0e0aa1c818e05c212715a36e8979
Arati <pradhan.arati14@gmail.com>
2020-03-18 20:43:22 +0530

    how to input the data is mentioned in comment line

--- d3-visualization/d3-visualization/scripts/data.js
+++ d3-visualization/d3-visualization/scripts/data.js
@@ -1,3 +1,6 @@
+//in type field input the type of bar u want to create
+//in name field input the name of the parameter u want to draw in x-axis
+//in value field input the value for which u want to draw the chart
 let data={"type":"bar",
 "parameter":[
   {
